9c02279ecae7c859474494e6aba8932c74bb8b68
apoorv-x12 <fifa.ajay219@gmail.comclear>
2021-08-22 22:39:11 +0530

    added ver3.txt added topics in views and changed settings.py also added datetime to topics and used context in render and {{}} in html file

--- website/basics/views.py
+++ website/basics/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 def welcome(request):
     return HttpResponse('<h1 style="color:blue;">welcome bruh!</h1>')
 
 def topics(request):
-    return render(request,'topics.html')
+    c=str(datetime.datetime.now())
+    return render(request,'topics.html',{'nowVar':c})
